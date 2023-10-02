6c8d974da817a6f1c02aea016e3e6aba544a1a38
Brian <brian.kawa@student.moringaschool.com>
2023-10-02 11:36:14 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    let cat = cats.slice();
+    cat.push("Broom");
+    return cat;
+}
+function prependCat(){
+    let cat = cats.slice();
+    cat.unshift("Arnold")
+    return cat;
+}
+function removeLastCat(){
+    let cat = cats.slice();
+    cat.pop();
+    return cat;
+}
+function removeFirstCat(){
+    let cat = cats.slice();
+    cat.shift();
+    return cat;
+}
